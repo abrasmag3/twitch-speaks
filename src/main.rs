143e044585ebb4757375211b9dd607a48c2c8f49
Abras <abrasmage@protonmail.com>
2022-12-13 18:47:37 -0500

    Revert Error & Shutdown Logic

--- src/main.rs
+++ src/main.rs
@@ -34,101 +34,76 @@ impl MessageData {
     }
 }
 
-// forks that change the behavior will likely edit this fn
-async fn manage_messages(
-    mut incoming_messages: mpsc::UnboundedReceiver<ServerMessage>,
-    _shutdown: mpsc::Sender<()>,
-) -> Result<(), tts::Error> {
-    
-    tracing::info!("Starting manage_messages");
-    // BTreeMap of recent messages, sorted by message content
-    // BTreeMap<Message, Vec<UserID>>
-    // this is used to decide which messages to speak
-    let mut messages: BTreeMap<String, MessageData> = BTreeMap::new();
-    // link to system tts
-    //TODO! exit program early if the system tts
-    // does not have the required features
-    //TODO! use mimic3 instead of the system tts
-    let mut tts = Tts::default()?;
-    tracing::info!("TTS initialized");
-
-    // gets each message one at a time
-    while let Some(message) = incoming_messages.recv().await {
-        tracing::info!("Recieved message");
-        //if message is a live chat message
-        match message {
-            ServerMessage::Privmsg(msg) => {
-                // add message to map
-                let text_normalized = msg.message_text.as_str().to_lowercase();
-
-                // if message is not in map, add it
-                //TODO! consider fuzzy finding
-                if !messages.contains_key(&text_normalized) {
-                    messages.insert(text_normalized, MessageData::new(msg.sender.id));
-                } else {
-                    // if message is in map, check if this user is in map
-                    // this unwrap cannot fail because we just checked that it contains this key
-                    let users = &mut messages.get_mut(&text_normalized).unwrap().users;
-                    // if not, add them
-                    if !users.contains(&msg.sender.id) {
-                        users.push(msg.sender.id);
-                    }
-                }
-            }
-            _ => {}
-        }
-
-        // handle messages
-        //TODO! is there a better way to do this?
-        let mut deleted_messages: Vec<String> = Vec::new();
-        for (message, data) in &messages {
-            // if message is popular, speak it with tts
-            if data.users.len() >= USER_THRESHOLD {
-                println!("Speaking \"{}\"", message);
-                deleted_messages.push(message.clone());
-                tts.speak(message, false)?;
-            }
-
-            // if message is old, remove it
-            if data.modified.elapsed().as_secs() >= MESSAGE_LIFETIME {
-                deleted_messages.push(message.clone());
-            }
-        }
-
-        for message in deleted_messages {
-            messages.remove(&message);
-        }
-    }
-    println!("returning");
-    //TODO! should we return an error if there are no more messages?
-    Ok(())
-}
-
 #[tokio::main]
 pub async fn main() -> Result<(), tts::Error> {
     tracing_subscriber::fmt::init();
-    // create shutdown start channel and shutdown end channel
-    let (shutdown_send, mut shutdown_recv) = broadcast::channel(2);
-    let (drop_to_shutdown, mut error_to_shutdown) = mpsc::channel(1);
-    
+
     // default configuration is to join chat as anonymous
     let config = ClientConfig::default();
-    let (incoming_messages, client) =
+    let (mut incoming_messages, client) =
         TwitchIRCClient::<SecureTCPTransport, StaticLoginCredentials>::new(config);
-    
-    // select! runs two functions and returns the result of the first one that ends
-    // the shutdown function will always end first unless there is an error,
-    // but when it ends it will stop the message manager function
-    tokio::select! {
-        // if an error is recieved, return the error
-        // we could just move `drop_to_shutdown` into the fn, but i prefer
-        // each thread to have a Sender
-        res = manage_messages(incoming_messages, drop_to_shutdown.clone()) => res?,
-        _ = shutdown_recv.recv() => {
-            // if a shutdown signal is recieved, return
-            return Ok(());
+
+    tokio::spawn(async move {
+        tracing::info!("Starting manage_messages");
+        // BTreeMap of recent messages, sorted by message content
+        // BTreeMap<Message, Vec<UserID>>
+        // this is used to decide which messages to speak
+        let mut messages: BTreeMap<String, MessageData> = BTreeMap::new();
+        // link to system tts
+        //TODO! exit program early if the system tts
+        // does not have the required features
+        //TODO! use mimic3 instead of the system tts
+        let mut tts = Tts::default().unwrap();
+        tracing::info!("TTS initialized");
+
+        // gets each message one at a time
+        while let Some(message) = incoming_messages.recv().await {
+            tracing::info!("Recieved message");
+            //if message is a live chat message
+            match message {
+                ServerMessage::Privmsg(msg) => {
+                    // add message to map
+                    let text_normalized = msg.message_text.as_str().to_lowercase();
+
+                    // if message is not in map, add it
+                    //TODO! consider fuzzy finding
+                    if !messages.contains_key(&text_normalized) {
+                        messages.insert(text_normalized, MessageData::new(msg.sender.id));
+                    } else {
+                        // if message is in map, check if this user is in map
+                        // this unwrap cannot fail because we just checked that it contains this key
+                        let users = &mut messages.get_mut(&text_normalized).unwrap().users;
+                        // if not, add them
+                        if !users.contains(&msg.sender.id) {
+                            users.push(msg.sender.id);
+                        }
+                    }
+                }
+                _ => {}
+            }
+
+            // handle messages
+            //TODO! is there a better way to do this?
+            let mut deleted_messages: Vec<String> = Vec::new();
+            for (message, data) in &messages {
+                // if message is popular, speak it with tts
+                if data.users.len() >= USER_THRESHOLD {
+                    println!("Speaking \"{}\"", message);
+                    deleted_messages.push(message.clone());
+                    tts.speak(message, false).unwrap();
+                }
+
+                // if message is old, remove it
+                if data.modified.elapsed().as_secs() >= MESSAGE_LIFETIME {
+                    deleted_messages.push(message.clone());
+                }
+            }
+
+            for message in deleted_messages {
+                messages.remove(&message);
+            }
         }
-    };
+    });
 
     //TODO! consider making client management and the ctrl+c handler seperate
 
@@ -144,16 +119,6 @@ pub async fn main() -> Result<(), tts::Error> {
 
     // leave the twitch channel
     client.part(STREAMER.to_owned());
-
-    // tell other threads to shutdown
-    shutdown_send.send(1).unwrap();
-
-    // let other threads know we are ready to shutdown
-    drop(drop_to_shutdown);
-
-    // When every sender has gone out of scope, the recv call
-    // will return with an error. We ignore the error.
-    let _ = error_to_shutdown.recv().await;
 
     Ok(())
 }
